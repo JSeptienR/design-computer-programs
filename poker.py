0a390ce7aa02f7cade62788a9e3edb62aa9a36e1
jseptien <septien.jorge@gmail.com>
2016-10-28 11:28:10 -0700

    Initial commit

---
+++ poker.py
@@ -0,0 +1,17 @@
+"""
+Follow Throguh Design of Computer Programs in Coursera
+"""
+
+def poker(hands):
+    pass
+
+
+def hand_rank():
+    pass
+
+def test():
+    pass
+
+
+if __name__ == '__main__':
+    test()
